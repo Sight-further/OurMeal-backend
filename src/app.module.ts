89f03016af236c7e363f7bb5cf6b43e47c355d6f
Flag_Fan <96718183+FlagFan34272@users.noreply.github.com>
2024-08-30 00:45:04 +0900

    firet

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MealsController } from './meals/meals.controller';
 import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { DataService } from './auth/db/data.service';
 
 @Module({
   imports: [],
   controllers: [AppController, MealsController, AuthController],
-  providers: [AppService],
+  providers: [AppService, AuthService, DataService],
 })
 export class AppModule {}
